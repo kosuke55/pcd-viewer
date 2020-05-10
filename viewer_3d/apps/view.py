8ab8e01be9b502c56c11813b516d4ff8a0fd8087
Your Name <kosuke.tnp@gmail.com>
2020-05-10 16:33:50 +0900

    Show origin

--- viewer_3d/apps/view.py
+++ viewer_3d/apps/view.py
@@ -1,7 +1,8 @@
 import argparse
+import os
+
 import numpy as np
 import open3d as o3d
-import os
 import skrobot
 import trimesh
 
@@ -14,6 +15,9 @@ def main():
     parser.add_argument('--channel', '-c', type=int,
                         help='Channel of pcd data.',
                         default=4)
+    parser.add_argument('--origin_size', '-os', type=float,
+                        help='origin size. if 0, no origin. (only when visualizing pointcloud)',
+                        default=1)
     args = parser.parse_args()
     root, ext = os.path.splitext(args.input)
 
@@ -22,11 +26,23 @@ def main():
         points = points_flatten.reshape(-1, args.channel)
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(points[:, :3])
-        o3d.visualization.draw_geometries([pcd])
+        if args.origin_size:
+            origin \
+                = o3d.geometry.TriangleMesh.create_coordinate_frame(
+                    size=args.origin_size)
+            o3d.visualization.draw_geometries([pcd, origin])
+        else:
+            o3d.visualization.draw_geometries([pcd])
 
     elif 'pcd' in ext.lower():
         pcd = o3d.io.read_point_cloud(args.input)
-        o3d.visualization.draw_geometries([pcd])
+        if args.origin_size:
+            origin \
+                = o3d.geometry.TriangleMesh.create_coordinate_frame(
+                    size=args.origin_size)
+            o3d.visualization.draw_geometries([pcd, origin])
+        else:
+            o3d.visualization.draw_geometries([pcd])
 
     elif 'off' in ext.lower() or'ply' in ext.lower() or 'stl' in ext.lower():
         obj = skrobot.model.Link()
